31386eb133be645665af7196e48846c622bb5006
Srinidhi-Sarthy <srinidhisarthy@gmail.com>
2026-01-19 11:54:31 -0600

    intake constants and subsystem with talonSRX motor and base commands

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+public class Constants {
+    public static class Intake {
+        public static final int ON = 5;
+        public static final int OFF = 0;
+    }
+}
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,27 @@
 package frc.robot.subsystems;
 
-public class Intake {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
+public class Intake extends SubsystemBase {
+    private final TalonSRX motor;
+
+    public Intake() {
+        this.motor = new TalonSRX(0);
+    }
+
+    public Command off(){
+        return this.runOnce(() -> {
+            this.motor.set(ControlMode.PercentOutput, Constants.Intake.OFF);
+        });
+    }
+
+    public Command on(){
+        return this.runOnce(() -> {
+            this.motor.set(ControlMode.PercentOutput, Constants.Intake.ON);
+        });
+    }
 }
