732feab004032fc2af4b066083924b2d9e65eaea
ShauryaVatsal <vatshaurya@gmail.com>
2026-01-19 15:20:53 -0600

    Changed talon srx to victor spx

--- src/main/java/org/team9140/frc2026/subsystems/Intake.java
+++ src/main/java/org/team9140/frc2026/subsystems/Intake.java
@@ -3,16 +3,17 @@ package org.team9140.frc2026.subsystems;
 import org.team9140.frc2026.Constants;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
     private static Intake instance;
-    private final TalonSRX motor;
+    private final VictorSPX motor;
 
     private Intake() {
-        this.motor = new TalonSRX(Constants.Ports.INTAKE_MOTOR);
+        this.motor = new VictorSPX(Constants.Ports.INTAKE_MOTOR);
     }
 
     public Command off() {
